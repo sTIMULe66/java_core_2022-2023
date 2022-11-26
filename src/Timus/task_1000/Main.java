524efaf94ca842bee67a42624a6a8b802634f98d
sTIMULe66 <stimul.e66@gmail.com>
2022-11-26 14:30:41 +0500

    Initial commit

--- src/Timus/task_1000/Main.java
+++ src/Timus/task_1000/Main.java
@@ -1,4 +1,21 @@
+/*
+Задание:
+https://acm.timus.ru/problem.aspx?space=1&num=1000
+ */
+
 package Timus.task_1000;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+
+        int c = a + b;
+
+        System.out.println(c);
+    }
 }
--- src/Timus/task_1293/Main.java
+++ src/Timus/task_1293/Main.java
@@ -1,4 +1,21 @@
+/*
+Задание:
+https://acm.timus.ru/problem.aspx?space=1&num=1293
+ */
+
 package Timus.task_1293;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int s = n * (a * b) *2;
+
+        System.out.println(s);
+    }
 }
